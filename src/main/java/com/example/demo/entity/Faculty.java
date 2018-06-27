d9b7160e33f48262003e4e30a5440ea6b4643ac2
Ali <alinorbobaev97@mail.ru>
2018-06-27 16:57:53 +0300

    создал сущность Факультет + связал с сущностью кафедра

---
+++ src/main/java/com/example/demo/entity/Faculty.java
@@ -0,0 +1,43 @@
+package com.example.demo.entity;
+
+import javax.persistence.*;
+
+@Entity // This tells Hibernate to make a table out of this class
+@Table(name = "faculty", catalog = "alinorbobaevdb")
+public class Faculty {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @JoinColumn(name = "department_id", nullable = false)
+
+    private Department department;
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+}
