a279b156ec9d9232253045bfcfe62d5830517fee
Ali <alinorbobaev97@mail.ru>
2018-06-29 12:25:47 +0300

    рабочие тесты для t-30 + исправил ошибку с выводом списка студентов у rest-контроллера

--- src/main/java/com/example/demo/controllers/StudentRestController.java
+++ src/main/java/com/example/demo/controllers/StudentRestController.java
@@ -4,9 +4,7 @@ import com.example.demo.entity.Student;
 import com.example.demo.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import org.springframework.web.bind.annotation.PathVariable;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,4 +20,8 @@ public class StudentRestController {
         return studentRepository.findAll();
     }
 
+    @GetMapping(path = "/find/{id}")
+    public Student getStudent(@PathVariable("id")Long id){
+        return studentRepository.getById(id);
+    }
 }
